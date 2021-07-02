11c168516796640f2103747e10e3905d276e596f
Lui Yong Sheng <13586871+luiyongsheng@users.noreply.github.com>
2021-07-02 08:52:35 +0800

    chore: webauthn credential model

---
+++ src/Models/WebAuthnCredential.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LuiYongSheng\WebAuthn\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class WebAuthnCredential extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'webauthn_credentials';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'credId', 'key',
+    ];
+}
