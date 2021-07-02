cc782b9bf39cdc3e97c6cf314273777ce34e3ae3
Lui Yong Sheng <13586871+luiyongsheng@users.noreply.github.com>
2021-07-02 08:53:12 +0800

    chore: utility to obtain credential source

---
+++ src/Utils/CredentialSource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LuiYongSheng\WebAuthn\Utils;
+
+use Illuminate\Support\Str;
+use Webauthn\TrustPath\EmptyTrustPath;
+use Webauthn\PublicKeyCredentialSource;
+use M1guelpf\FastLogin\Models\WebAuthnCredential;
+use Webauthn\PublicKeyCredentialUserEntity;
+use Webauthn\PublicKeyCredentialSourceRepository;
+
+class CredentialSource implements PublicKeyCredentialSourceRepository
+{
+    public function findOneByCredentialId(string $publicKeyCredentialId): ?PublicKeyCredentialSource
+    {
+        $credential = WebAuthnCredential::where('credId', $publicKeyCredentialId)->first();
+
+        return is_null($credential) ? $credential : new PublicKeyCredentialSource(
+            $credential['credId'],
+            'public-key',
+            ['internal'],
+            'none',
+            new EmptyTrustPath,
+            Str::uuid(),
+            $credential['key'],
+            $credential->user_id,
+            0,
+        );
+    }
+
+    public function findAllForUserEntity(PublicKeyCredentialUserEntity $publicKeyCredentialUserEntity): array
+    {
+        return [];
+    }
+
+    public function saveCredentialSource(PublicKeyCredentialSource $publicKeyCredentialSource): void
+    {
+    }
+}
